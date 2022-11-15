b3e439ab664173eba0baa8502ca6551b35227871
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 19:27:15 +0300

    Продолжаем ООП, но про автомобили

--- src/Car.java
+++ src/Car.java
@@ -3,10 +3,15 @@ public class Car {
     String model;
     float engineVolume;
     String color;
-    String productionYear;
+    int productionYear;
     String country;
 
-    void introduceCar(String brand, String model, float engineVolume, String color, String productionYear, String country) {
-
+    void introduceCar() {
+        System.out.println("Бренд " + brand);
+        System.out.println("Марка " + model);
+        System.out.println("Объём двигателя - " + engineVolume);
+        System.out.println("Цвет " + color);
+        System.out.println("Год производства " + productionYear);
+        System.out.println("Страна производителя " + country);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.country = "Чёрти где";
+        car.color = "Вроде бы красный, но больше зелёный";
+        car.brand = "Тачанка";
+        car.model = "На болтах";
+        car.productionYear = 1999;
+        car.engineVolume = 0.5f;
+        car.introduceCar();
+    }
 }
