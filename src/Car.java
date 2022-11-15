538a290c0629007684918615bed0c7efa6e4f69f
YuryShageev <schageev.yur@yandex.ru>
2022-11-15 17:52:29 +0300

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    String brand;
+    String model;
+    float engineVolume;
+    String color;
+    String productionYear;
+    String country;
+
+    void introduceCar(String brand, String model, float engineVolume, String color, String productionYear, String country) {
+
+    }
+}
