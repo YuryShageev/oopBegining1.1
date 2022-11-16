a7be26964335fee7665432ef5100702c398fd717
YuryShageev <schageev.yur@yandex.ru>
2022-11-16 15:19:25 +0300

    Продолжаем ООП, но про автомобили

--- src/Car.java
+++ src/Car.java
@@ -6,6 +6,24 @@ public class Car {
     int productionYear;
     String country;
 
+    Car(String brand, String model, int year, String country, String color, float engineVolume) {
+        this.brand = brand;
+        this.model = model;
+        this.productionYear = year;
+        this.country = country;
+        this.color = color;
+        this.engineVolume = engineVolume;
+    }
+
+    Car() {
+        brand = "Гнилая";
+        model = "тачанка";
+        productionYear = 1960;
+        country = "Черти где";
+        color = "Такой же как красный, только синий";
+        engineVolume = 1.1f;
+    }
+
     void introduceCar() {
         System.out.println("Бренд " + brand);
         System.out.println("Марка " + model);
@@ -13,5 +31,10 @@ public class Car {
         System.out.println("Цвет " + color);
         System.out.println("Год производства " + productionYear);
         System.out.println("Страна производителя " + country);
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", " + productionYear + " года выпуска, сборка в стране " + country + ", " + color + " цвета, объем двигателя - " + engineVolume + "л.";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,13 @@ public class Main {
         car.productionYear = 1999;
         car.engineVolume = 0.5f;
         car.introduceCar();
+
+        Car car1 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", " чёрного", 3.0f);
+
+        Car car2 = new Car("BMW", "Z8", 2021, "Германия", " чёрного", 3.0f);
+
+        Car car3 = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красного", 2.4f);
+
+        Car car4 = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевого", 1.6f);
     }
 }
