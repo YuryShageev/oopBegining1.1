399c87ff1f5b7eef0d39fc7f5c00578d2a4d0f41
YuryShageev <schageev.yur@yandex.ru>
2022-11-16 15:20:36 +0300

    Продолжаем ООП, но про автомобили

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,12 @@ public class Main {
         car.introduceCar();
 
         Car car1 = new Car("Audi", "A8 50 L TDI quattro", 2020, "Германия", " чёрного", 3.0f);
-
+        System.out.println(car1);
         Car car2 = new Car("BMW", "Z8", 2021, "Германия", " чёрного", 3.0f);
-
+        System.out.println(car2);
         Car car3 = new Car("Kia", "Sportage 4-го поколения", 2018, "Южная Корея", "красного", 2.4f);
-
+        System.out.println(car3);
         Car car4 = new Car("Hyundai", "Avante", 2016, "Южная Корея", "оранжевого", 1.6f);
+        System.out.println(car4);
     }
 }
